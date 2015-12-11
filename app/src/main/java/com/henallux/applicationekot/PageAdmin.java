1475bc1e8b6932f77262aba991c2e5a93ddce374
frantzc01@gmail.com <frantzc01@gmail.com>
2015-12-11 11:41:18 +0100

    PageAdmin+LogAdmin+ModifAccueil

---
+++ app/src/main/java/com/henallux/applicationekot/PageAdmin.java
@@ -0,0 +1,24 @@
+package com.henallux.applicationekot;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by etu22360 on 11/12/2015.
+ */
+public class PageAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pageadmin);
+
+
+    }
+
+}
+
